d1de8180457074e751700744c37708b42ec1541a
Loana Albisser <loana.albisser@bluewin.ch>
2015-05-05 12:21:41 +0200

    update Settings

--- app/src/main/java/mobpro/hslu/poker_operator/settings/SettingsCurrency.java
+++ app/src/main/java/mobpro/hslu/poker_operator/settings/SettingsCurrency.java
@@ -1,19 +1,29 @@
 package mobpro.hslu.poker_operator.settings;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import mobpro.hslu.poker_operator.R;
 
 /**
  * Created by User on 04.05.2015.
  */
-public class SettingsCurrency extends Activity implements AdapterView.OnItemClickListener {
-    private String[] aData = new String[] {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6" };
+public class SettingsCurrency extends Activity  {
+    private ListView listView;
+    private ArrayList<String> array;
+    private ArrayAdapter<String>adapter;
+    private Button addButton;
 
     public SettingsCurrency(){
 
@@ -23,16 +33,41 @@ public class SettingsCurrency extends Activity implements AdapterView.OnItemClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_currency);
 
-        ListView listView = (ListView)findViewById(R.id.listView_currney);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, aData);
+        array = new ArrayList<>();
+        listView = (ListView)findViewById(R.id.listView_currney);
+        for (int i=0; i<2;i++){
+            array.add("blabla"+i);
+        }
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, array);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void addCurrency (View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setTitle("Add Currency");
+        builder.setView(input);
 
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, array);
+        listView.setAdapter(adapter);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                adapter.add(input.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
+
 }
 
--- app/src/main/java/mobpro/hslu/poker_operator/settings/SettingsLimit.java
+++ app/src/main/java/mobpro/hslu/poker_operator/settings/SettingsLimit.java
@@ -1,7 +1,16 @@
 package mobpro.hslu.poker_operator.settings;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import mobpro.hslu.poker_operator.R;
 
@@ -9,6 +18,9 @@ import mobpro.hslu.poker_operator.R;
  * Created by User on 04.05.2015.
  */
 public class SettingsLimit extends Activity{
+    private ListView listView;
+    private ArrayList<String> array;
+    private ArrayAdapter<String> adapter;
     public SettingsLimit(){
 
     }
@@ -16,6 +28,41 @@ public class SettingsLimit extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_limit);
+
+        array = new ArrayList<>();
+        listView = (ListView)findViewById(R.id.listView_limit);
+        for (int i=0; i<2;i++){
+            array.add("blabla"+i);
+        }
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, array);
+        listView.setAdapter(adapter);
+    }
+
+    public void addLimit (View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        builder.setTitle("Add Limit");
+        builder.setView(input);
+
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, array);
+        listView.setAdapter(adapter);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                adapter.add(input.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 }
 
