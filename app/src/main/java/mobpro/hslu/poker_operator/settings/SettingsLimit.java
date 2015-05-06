2eeaf648dfe5ad2e308ad2f54d40870dda756d46
tbeugste <andreas.eugster@stud.hslu.ch>
2015-05-06 15:18:50 +0200

    Implemented DB-Creation+access

--- app/src/main/java/mobpro/hslu/poker_operator/settings/SettingsLimit.java
+++ app/src/main/java/mobpro/hslu/poker_operator/settings/SettingsLimit.java
@@ -11,8 +11,11 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import mobpro.hslu.poker_operator.R;
+import mobpro.hslu.poker_operator.database.DbAdapter;
+import mobpro.hslu.poker_operator.entity.Limittype;
 
 /**
  * Created by User on 04.05.2015.
@@ -21,12 +24,18 @@ public class SettingsLimit extends Activity{
     private ListView listView;
     private ArrayList<String> array;
     private ArrayAdapter<String> adapter;
+    DbAdapter dbAdapter;
+
+    private ArrayList<Limittype> allLimittypes;
+    private ArrayAdapter<Limittype> limittypeArrayAdapter;
+
     public SettingsLimit(){
 
     }
+    /*
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        dbAdapter = new DbAdapter(this);
         setContentView(R.layout.settings_limit);
 
         array = new ArrayList<>();
@@ -36,6 +45,34 @@ public class SettingsLimit extends Activity{
         }
         adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, array);
         listView.setAdapter(adapter);
+
+        super.onCreate(savedInstanceState);
+    }*/
+
+    public void onCreate(Bundle savedInstanceState) {
+        dbAdapter = new DbAdapter(getApplicationContext());
+        dbAdapter.open();
+        setContentView(R.layout.settings_limit);
+
+        listView = (ListView)findViewById(R.id.listView_limit);
+        allLimittypes = new ArrayList<>(Limittype.getAllLimittypes(dbAdapter));
+
+        limittypeArrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, allLimittypes);
+        listView.setAdapter(limittypeArrayAdapter);
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume(){
+        dbAdapter.open();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        dbAdapter.close();
+        super.onPause();
     }
 
     public void addLimit (View v){
